8579c466325abb80c8af845dcb9120ba44793d93
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-11-28 19:25:02 +0530

    MainActivity added

--- app/src/main/java/com/example/collegemanager/MainActivity.java
+++ app/src/main/java/com/example/collegemanager/MainActivity.java
@@ -2,9 +2,23 @@ package com.example.collegemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Login as Admin Activity's Button
+    public void conAsAdmin(View v){
+        Intent intent = new Intent(MainActivity.this, AdminLoginActivity.class);
+        startActivity(intent);
+    }
+
+    // Continue as user Activity's Button
+    public void conAsStudent(View v){
+        Intent intent = new Intent(MainActivity.this, StudentSelectActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
